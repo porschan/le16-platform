2aeea4110b3822addcfd0590a5468f502ba73746
porschan <710437653@qq.com>
2018-11-14 23:09:33 +0800

    First commit

---
+++ src/main/java/com/chanchifeng/le16/Le16Application.java
@@ -0,0 +1,12 @@
+package com.chanchifeng.le16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Le16Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Le16Application.class, args);
+    }
+}
---
+++ src/test/java/com/chanchifeng/le16/Le16ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chanchifeng.le16;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Le16ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
